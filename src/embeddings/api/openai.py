fd34e350772498f57c52167bae58f3359dc0d0c3
foryourselfand <foryourselfand@gmail.com>
2024-04-14 00:35:10 +0300

    splitting with tiktoken

--- src/embeddings/api/openai.py
+++ src/embeddings/api/openai.py
@@ -1,6 +1,9 @@
+from typing import List
+
 import chromadb
 from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 from fastapi import APIRouter, Query
+from langchain_text_splitters import TokenTextSplitter
 
 from embeddings.api.schema import EmbeddingRequest, SearchRequest
 from embeddings.infrastructure.chroma_db_config import chroma_db_config
@@ -11,23 +14,44 @@ openai_router = APIRouter(
     tags=["openai"],
 )
 
+text_splitter = TokenTextSplitter.from_tiktoken_encoder(
+    encoding_name=external_api_config.OPENAI_MODEL_ENCODER_NAME,
+    model_name=external_api_config.OPENAI_MODEL_NAME,
+    chunk_size=external_api_config.OPENAI_MODEL_MAX_INPUT,
+    chunk_overlap=0
+)
+
 chroma_client = chromadb.HttpClient(host=chroma_db_config.HOST, port=chroma_db_config.PORT)
 
-openai_embedding_function = OpenAIEmbeddingFunction(api_key=external_api_config.OPENAI_TOKEN, model_name=external_api_config.OPENAI_MODEL)
+openai_embedding_function = OpenAIEmbeddingFunction(api_key=external_api_config.OPENAI_TOKEN, model_name=external_api_config.OPENAI_MODEL_NAME)
 openai_collection = chroma_client.get_or_create_collection(name='openai_collection', embedding_function=openai_embedding_function)
 
 
 @openai_router.post(
-    "/embedding",
+    "/embeddings",
 )
 async def save_embedding(
-    request: EmbeddingRequest,
+    request: List[EmbeddingRequest],
 ):
+    new_entries = [
+        (
+            f"{item.id}_{idx}",
+            chunk,
+            {"source": item.source, "part_index": idx}
+        )
+        for item in request
+        for idx, chunk in enumerate(text_splitter.split_text(item.text))
+    ]
+
+    new_ids, new_documents, new_metadatas = zip(*new_entries)
+
     openai_collection.add(
-        ids=[request.id],
-        documents=[request.text],
-        metadatas=[{"source": request.source}],
+        ids=list(new_ids),
+        documents=list(new_documents),
+        metadatas=list(new_metadatas),
     )
+
+    return {"message": "Embeddings saved"}
 
 
 @openai_router.post(
--- src/embeddings/infrastructure/external_api_config.py
+++ src/embeddings/infrastructure/external_api_config.py
@@ -6,7 +6,9 @@ class ExternalApiConfig(BaseSettings):
     model_config = SettingsConfigDict(env_prefix='EXTERNAL_API_')
 
     OPENAI_TOKEN: str = Field(...)
-    OPENAI_MODEL: str = Field("text-embedding-3-large")
+    OPENAI_MODEL_NAME: str = Field("text-embedding-3-large")
+    OPENAI_MODEL_ENCODER_NAME: str = Field("cl100k_base")
+    OPENAI_MODEL_MAX_INPUT: int = Field(8191)
 
 
 external_api_config = ExternalApiConfig()
